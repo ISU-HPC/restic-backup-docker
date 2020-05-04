8ee28d35080cdda88dfc597abf249af6ce3700d0
David Orman <orman@iastate.edu>
2020-05-04 15:57:53 -0500

    added check for existing process and fuse for mounted restores

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 echo "Removing old container names 'backup-test' if exists"
-docker rm -f -v backup-test || true
+sudo docker rm -f -v backup-test || true
 
 echo "Start backup-test container. Backup of ~/test-data/ to repository ~/test-repo/ every minute"
-docker run --privileged --name backup-test \
+sudo docker run --privileged --name backup-test \
 -e "RESTIC_PASSWORD=test" \
 -e "RESTIC_TAG=test" \
 -e "BACKUP_CRON=* * * * *" \
